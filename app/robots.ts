91cbd4cbaac693e06db8b5952c1709bbcb1580e0
Rey Cannavaro <reyjunoalcannavaro@gmail.com>
2026-05-20 15:37:33 +0700

    push image for favicon

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_APP_URL ?? "https://findor.id";
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL ?? "https://findor-azure.vercel.app/";
 
   return {
     rules: [
